0b3e1a88770584802963e724a2e1e386723c8dc1
yikl1004 <yikl1004@gmail.com>
2016-04-27 09:31:36 +0900

    aa
    aaa

--- front/com/js/cm.beautysquare.ui.js
+++ front/com/js/cm.beautysquare.ui.js
@@ -300,54 +300,54 @@ $(function( event ){
 	};
 
 	// long tap contextmenu 방지
-	// (function(){
-	// 	var cancel=function(e){
-	// 		if (window.event) {
-	// 			window.event.cancelBubble = true;
-	// 			window.event.returnValue = false;
-	// 		}
-	// 		if (e && e.stopPropagation && e.preventDefault) {
-	// 			e.stopPropagation();
-	// 			e.preventDefault();
-	// 		}
-	// 		return false;
-	// 	};
-	// 	var block=function(e){
-	// 		e = e || window.event;
-	// 		var t=e.srcElement || e.target;
-	// 		var tag=t.tagName;
-	// 		if (e && tag==='HTML' || tag==='INPUT' || tag==='TEXTAREA' || tag==='BUTTON' || tag==='SELECT' || tag==='OPTION' || tag==='EMBED' || tag==='OBJECT') { return; }
-	// 		if (e.type==='keydown' || e.type=='keyup') {
-	// 			// keyboard event : only block ctrl-A, ctrl-a, ctrl-C, ctrl-c, meta-A, meta-a, meta-C, meta-c
-	// 			if ((e.ctrlKey || e.metaKey) && (e.keyCode == 65 || e.keyCode == 97 || e.keyCode == 67 || e.keyCode == 99)) { return cancel(e); }
-	// 		} else if(e.type == "contextmenu"){
-	// 			console.log('무단복사를 막기 위해 마우스 드래그 금지가 설정되어 있습니다');
-	// 			return cancel(e);
-	// 		} else {
-	// 			return cancel(e);
-	// 		}
-	// 	}
-	// 	var addEvent = function(el, type, fn){
-	// 		if (window.addEventListener) {
- // 				el.addEventListener(type, fn, false);
-	// 		} else if (window.attachEvent) {
-	// 			el.attachEvent('on' + type, fn);
-	// 		} else {
-	// 			el['on' + type] = fn;
-	// 		}
-	// 	}
-	// 	var addBlockEvent = function(){
-	// 		addEvent(document.body,'keydown',block);
-	// 		addEvent(document.body,'keyup',block);
-	// 		addEvent(document.body,'mouseup',block);
-	// 		addEvent(document.body,'mousedown',block);
-	// 		addEvent(document.body,'dragstart',block);
-	// 		addEvent(document.body,'selectstart',block);
-	// 		addEvent(document.body,'copy',block);
-	// 		addEvent(document.body,'contextmenu', block);
-	// 	};
-	// 	addEvent(window,'load',addBlockEvent);
-	// })();
+	(function(){
+		var cancel=function(e){
+			if (window.event) {
+				window.event.cancelBubble = true;
+				window.event.returnValue = false;
+			}
+			if (e && e.stopPropagation && e.preventDefault) {
+				e.stopPropagation();
+				e.preventDefault();
+			}
+			return false;
+		};
+		var block=function(e){
+			e = e || window.event;
+			var t=e.srcElement || e.target;
+			var tag=t.tagName;
+			if (e && tag==='HTML' || tag==='INPUT' || tag==='TEXTAREA' || tag==='BUTTON' || tag==='SELECT' || tag==='OPTION' || tag==='EMBED' || tag==='OBJECT') { return; }
+			if (e.type==='keydown' || e.type=='keyup') {
+				// keyboard event : only block ctrl-A, ctrl-a, ctrl-C, ctrl-c, meta-A, meta-a, meta-C, meta-c
+				if ((e.ctrlKey || e.metaKey) && (e.keyCode == 65 || e.keyCode == 97 || e.keyCode == 67 || e.keyCode == 99)) { return cancel(e); }
+			} else if(e.type == "contextmenu"){
+				console.log('무단복사를 막기 위해 마우스 드래그 금지가 설정되어 있습니다');
+				return cancel(e);
+			} else {
+				return cancel(e);
+			}
+		}
+		var addEvent = function(el, type, fn){
+			if (window.addEventListener) {
+ 				el.addEventListener(type, fn, false);
+			} else if (window.attachEvent) {
+				el.attachEvent('on' + type, fn);
+			} else {
+				el['on' + type] = fn;
+			}
+		}
+		var addBlockEvent = function(){
+			addEvent(document.body,'keydown',block);
+			addEvent(document.body,'keyup',block);
+			addEvent(document.body,'mouseup',block);
+			addEvent(document.body,'mousedown',block);
+			addEvent(document.body,'dragstart',block);
+			addEvent(document.body,'selectstart',block);
+			addEvent(document.body,'copy',block);
+			addEvent(document.body,'contextmenu', block);
+		};
+		addEvent(window,'load',addBlockEvent);
+	})();
 
 
 
@@ -416,7 +416,7 @@ $(function( event ){
 			$(window).on('load', function(){
 				var gnbWidth = 0;
 				for ( var i=0; i<=$('#gnb li').length; i++ )
-					gnbWidth += $('#gnb li').eq(i).outerWidth(true);
+					gnbWidth += $('#gnb li').eq(i).outerWidth(true) + 1;
 
 				$('#gnb').width( gnbWidth + 1 );
 				$('#gnb li').eq(0).find('a').addClass('active');
