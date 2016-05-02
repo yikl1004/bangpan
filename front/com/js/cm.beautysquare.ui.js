04dbb20325e64b2b4f5add9c9b28802f96f1a531
yikl1004 <yikl1004@gmail.com>
2016-05-02 14:40:26 +0900

    aa
    aa

--- front/com/js/cm.beautysquare.ui.js
+++ front/com/js/cm.beautysquare.ui.js
@@ -994,6 +994,7 @@ $(function( event ){
 		if ( $('#wrap').find('> .app_download').length > 0 ) {
 			var downloadPath = {
 				android: 'https://dl.dropboxusercontent.com/s/k7ma5pkkbxskozn/beautySquare2.apk',
+				gingerbread: 'https://dl.dropboxusercontent.com/s/k7ma5pkkbxskozn/beautySquare2.apk',
 				ios: 'itms-services://?action=download-manifest&url=https://dl.dropboxusercontent.com/s/7sv68u0v7znuv2k/houseSelling.plist',
 				'no-mobile': 'javascript: alert("태블릿 또는 PC에서는 다운로드 할 수 없습니다.");'
 			};
